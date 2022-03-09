524c4f242a38e0a9de15c04c105d7c3e69373e58
Evangelos Poneres <eponeres@koniag-gs.com>
2022-03-09 15:10:25 -0500

    Accounting for schema updates

--- GENERATE_TEST_USER.sql
+++ GENERATE_TEST_USER.sql
@@ -1,17 +1,22 @@
 --Assumption is made that a CE SUPERVISOR account exists with the email 'ecomp+cesuper@intevity.com'
 
 create procedure deleteCeData
-    @USERNAME VARCHAR(255),
-    @GRADE INT,
-    @FISCAL_YEAR INT
+    @USERNAME VARCHAR(255)
 AS
     declare @USER_ID INT = (select top 1 user_id from [user] where [user_name] = @USERNAME);
     delete from ce_supervisor_dashboard_user where claims_examiner_id = @USER_ID;
     delete from [user] where user_id = @USER_ID;
-    delete from review where user_id = @USER_ID;
-    delete from review_summary where user_id = @USER_ID;
+
+    delete from timeliness_case where id in (select timelinessCases_ID from REVIEW_timeliness_case where Review_REVIEWID in (select review_id from review where review_period_id in (select id from review_period where review_evaluation_id in (select id from review_evaluation where user_id = @USER_ID))))
+    delete from REVIEW_timeliness_case where Review_REVIEWID in (select review_id from review where review_period_id in (select id from review_period where review_evaluation_id in (select id from review_evaluation where user_id = @USER_ID)))
+    delete from review_case_exclusion where review_id in (select review_id from review where review_period_id in (select id from review_period where review_evaluation_id in (select id from review_evaluation where user_id = @USER_ID)))
+    delete from review where review_period_id in (select id from review_period where review_evaluation_id in (select id from review_evaluation where user_id = @USER_ID))
+    delete from review_summary where review_period_id in (select id from review_period where review_evaluation_id in (select id from review_evaluation where user_id = @USER_ID))
+    delete from review_period where review_evaluation_id in (select id from review_evaluation where user_id = @USER_ID)
+    delete from review_evaluation where user_id = @USER_ID
+
     declare @EVAL_ID INT;
-    select @EVAL_ID = [id] from review_evaluation where user_id = @USER_ID and grade = @GRADE and fiscal_year = @FISCAL_YEAR;
+    select @EVAL_ID = [id] from review_evaluation where user_id = @USER_ID;
     delete rp
     from dbo.review_period rp
     join dbo.review_evaluation re
@@ -26,8 +31,11 @@ AS
     select * from [user] where user_id = @USER_ID
     select * from [claims_examiner] where user_id = @USER_ID
     select * from ce_supervisor_dashboard_user where claims_examiner_id = @USER_ID
-    select * from review where user_id = @USER_ID
-    select * from review_summary where user_id  = @USER_ID
+    select * from timeliness_case where id in (select id from REVIEW_timeliness_case where Review_REVIEWID in (select review_id from review where review_period_id in (select id from review_period where review_evaluation_id in (select id from review_evaluation where user_id = @USER_ID))))
+    select * from review_case_exclusion where review_id in (select review_id from review where review_period_id in (select id from review_period where review_evaluation_id in (select id from review_evaluation where user_id = @USER_ID)))
+    select * from review where review_period_id in (select id from review_period where review_evaluation_id in (select id from review_evaluation where user_id = @USER_ID))
+    select * from review_summary where review_period_id in (select id from review_period where review_evaluation_id in (select id from review_evaluation where user_id = @USER_ID))
+    select * from review_period where review_evaluation_id in (select id from review_evaluation where user_id = @USER_ID)
     select * from review_evaluation where user_id = @USER_ID
 go
 
@@ -40,7 +48,7 @@ create procedure generateTestUser
     @PARTY_ID INTEGER
 as
 
-    exec deleteCeData @USERNAME, @GRADE, @FISCAL_YEAR
+    exec deleteCeData @USERNAME
 
     -- Add user as a Claims Examiner
     insert into [user](user_name, first_name, last_name, email_address) values (@USERNAME, @FIRSTNAME, @LASTNAME, 'ecomp+' + @USERNAME + '@intevity.com');
@@ -55,8 +63,7 @@ as
     -- Add review evaluation for fiscal year and user
     insert into review_evaluation (user_id, fiscal_year, grade, [start_date], review_type, ce_agreement_date, ce_supervisor_agreement_date, reviewing_officer_agreement_date, created_by)
     values(@USER_ID, @FISCAL_YEAR, @GRADE, CURRENT_TIMESTAMP, 'CE', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, @SUPERVISOR_USER_ID);
-    declare @EVAL_ID INT;
-    select @EVAL_ID = [id] from dbo.review_evaluation where [grade] = @GRADE and [user_id] = @USER_ID and [fiscal_year] = @FISCAL_YEAR and [close_date] IS NULL;
+    declare @EVAL_ID INT = (select TOP 1 [id] from dbo.review_evaluation where user_id = @USER_ID);
 
     -- Assign to supervisor dashboard
     insert into ce_supervisor_dashboard_user(ce_supervisor_id, claims_examiner_id) values(@SUPERVISOR_USER_ID, @USER_ID);
@@ -64,83 +71,81 @@ as
     create table #months (
         review_month INTEGER, 
         review_year INTEGER,
-        fiscal_year INTEGER,
-        review_status VARCHAR(255),
-        grade INTEGER
+        review_status VARCHAR(255)
     )
 
-    insert into #months (review_month, review_year, fiscal_year, review_status, grade)
-   
-    select 10, 2021, @FISCAL_YEAR, 'COMPLETED', @GRADE union
-    select 11, 2021, @FISCAL_YEAR, 'COMPLETED', @GRADE union
-    select 12, 2021, @FISCAL_YEAR, 'COMPLETED', @GRADE union
-    select 1, 2022, @FISCAL_YEAR, 'COMPLETED', @GRADE union
-    select 2, 2022, @FISCAL_YEAR, 'COMPLETED', @GRADE union
-    select 3, 2022, @FISCAL_YEAR, 'STARTED', @GRADE
+    insert into #months (review_month, review_year, review_status)
+    select 10, 2021, 'COMPLETED' union
+    select 11, 2021, 'COMPLETED' union
+    select 12, 2021, 'COMPLETED' union
+    select 1, 2022, 'COMPLETED' union
+    select 2, 2022, 'COMPLETED' union
+    select 3, 2022, 'STARTED'
+
+    -- Add Review Periods
+    insert into dbo.review_period
+    select review_month, review_year, @EVAL_ID
+    from #months;
 
     -- Add reviews
-    insert into review(user_id, review_month, review_year, fiscal_year, grade, review_type, review_section, element, component_id, review_status, total, ok)
-    select @USER_ID as user_id, m.review_month, m.review_year, m.fiscal_year, m.grade, 'CE' as review_type, 'TIMELINESS' as review_section, c.element_id as element, c.id, m.review_status, 25 as total, 10 as ok
+    insert into review(review_section, element, component_id, review_status, total, ok, review_period_id)
+    select distinct 'TIMELINESS' as review_section, c.element_id as element, c.id, m.review_status, 25 as total, 10 as ok, rp.id
     from dbo.lead_component c
     join dbo.lead_element_group eg on eg.element_id = c.element_id
     join dbo.lead_element_group_component egc on egc.component_id = c.id and egc.group_id = eg.group_id
     join dbo.review_evaluation re on re.user_id = @USER_ID and re.grade = @GRADE and re.fiscal_year = @FISCAL_YEAR
     full outer join #months m
-    on m.fiscal_year = egc.fiscal_year
-    and m.grade = egc.grade
-    where c.element_id is not null and m.grade = @GRADE
+    on 1 = 1
+    inner join review_period rp
+    on m.review_month = rp.review_month
+    and m.review_year = rp.review_year
+    and rp.review_evaluation_id = @EVAL_ID
+    where c.element_id is not null
     UNION ALL
-    select distinct @USER_ID as user_id, m.review_month, m.review_year, m.fiscal_year, m.grade, 'CE' as review_type, 'QUALITY' as review_section, a.lead_element_id as element, null, m.review_status, 25 as total, 10 as ok
+    select distinct 'QUALITY' as review_section, a.lead_element_id as element, null, m.review_status, 25 as total, 10 as ok, rp.id
     from lead_element a
     join dbo.review_evaluation re on re.user_id = @USER_ID and re.grade = @GRADE and re.fiscal_year = @FISCAL_YEAR
-    cross join #months m;
-
-    -- Add review periods
-    insert into dbo.review_period
-    select review_month, review_year, @EVAL_ID
-    from #months;
-
-    update r
-    set r.review_period_id = rp.id
-    from dbo.[review] r
-    join dbo.[review_evaluation] re on re.id = @EVAL_ID
-    join dbo.[review_period] rp 
-        on rp.review_evaluation_id = re.id 
-        and rp.review_month = r.review_month 
-        and rp.review_year = r.review_year
-    where r.user_id = @USER_ID and re.grade = @GRADE and re.fiscal_year = @FISCAL_YEAR;
+    full outer join #months m
+    on 1 = 1
+    inner join review_period rp
+    on m.review_month = rp.review_month
+    and m.review_year = rp.review_year
+    and rp.review_evaluation_id = @EVAL_ID
 
     drop table #months
 
     -- Add review summaries
-    insert into review_summary(user_id, quality_status, review_month, fiscal_year, grade, review_type, review_period_id, updated_by, updated_date)
-    select distinct user_id, review_status, review_month, fiscal_year, grade, review_type, review_period_id, @SUPERVISOR_USER_ID, CURRENT_TIMESTAMP
+    insert into review_summary(quality_status, review_period_id, updated_by, updated_date)
+    select distinct review_status, review_period_id, @SUPERVISOR_USER_ID, CURRENT_TIMESTAMP
     from review 
-    where user_id = @USER_ID
+    where review_period_id in (select id from review_period where review_evaluation_id in (select id from review_evaluation where id = @EVAL_ID))
     and review_section = 'QUALITY'
 
     update rs
     set rs.timeliness_status = r.review_status
     from review_summary rs
     join review r
-    on rs.review_type = r.review_type
-    and rs.review_month = r.review_month
-    and rs.fiscal_year = r.fiscal_year
-    and rs.grade = r.grade
-    and rs.user_id = r.user_id
-    where rs.user_id = @USER_ID
+    on rs.review_period_id = r.review_period_id
+    where rs.review_period_id in (select id from review_period where review_evaluation_id in (select id from review_evaluation where id = @EVAL_ID))
     and review_section = 'TIMELINESS'
 
     exec selectCeData @USERNAME
 
 go
 
-exec generateTestUser @USERNAME = 'jwick12', @FIRSTNAME = 'John12', @LASTNAME = 'Wick', @GRADE = 12, @FISCAL_YEAR = 2022, @PARTY_ID = 999;
-exec generateTestUser @USERNAME = 'jwick7', @FIRSTNAME = 'John7', @LASTNAME = 'Wick', @GRADE = 7, @FISCAL_YEAR = 2022, @PARTY_ID = 999;
-exec generateTestUser @USERNAME = 'bwayne', @FIRSTNAME = 'Bruce', @LASTNAME = 'Wayne', @GRADE = 7, @FISCAL_YEAR = 2022, @PARTY_ID = 888;
-exec generateTestUser @USERNAME = 'nromanov', @FIRSTNAME = 'Natasha', @LASTNAME = 'Romanov', @GRADE = 7, @FISCAL_YEAR = 2022, @PARTY_ID = 333;
-exec generateTestUser @USERNAME = 'tanderson', @FIRSTNAME = 'Thomas', @LASTNAME = 'Anderson', @GRADE = 12, @FISCAL_YEAR = 2022, @PARTY_ID = 777;
-exec generateTestUser @USERNAME = 'nportman', @FIRSTNAME = 'Natalie', @LASTNAME = 'Portman', @GRADE = 9, @FISCAL_YEAR = 2022, @PARTY_ID = 111;
+exec deleteCeData @USERNAME = 'epon8'
+exec deleteCeData @USERNAME = 'jwick7'
+exec deleteCeData @USERNAME = 'bwayne'
+exec deleteCeData @USERNAME = 'nromanov'
+exec deleteCeData @USERNAME = 'tanderson'
+exec deleteCeData @USERNAME = 'nportman'
+
+-- exec generateTestUser @USERNAME = 'epon8', @FIRSTNAME = 'Evan', @LASTNAME = 'Poe', @GRADE = 12, @FISCAL_YEAR = 2022, @PARTY_ID = 999;
+-- exec generateTestUser @USERNAME = 'jwick7', @FIRSTNAME = 'John7', @LASTNAME = 'Wick', @GRADE = 7, @FISCAL_YEAR = 2022, @PARTY_ID = 999;
+-- exec generateTestUser @USERNAME = 'bwayne', @FIRSTNAME = 'Bruce', @LASTNAME = 'Wayne', @GRADE = 7, @FISCAL_YEAR = 2022, @PARTY_ID = 888;
+-- exec generateTestUser @USERNAME = 'nromanov', @FIRSTNAME = 'Natasha', @LASTNAME = 'Romanov', @GRADE = 7, @FISCAL_YEAR = 2022, @PARTY_ID = 333;
+-- exec generateTestUser @USERNAME = 'tanderson', @FIRSTNAME = 'Thomas', @LASTNAME = 'Anderson', @GRADE = 12, @FISCAL_YEAR = 2022, @PARTY_ID = 777;
+-- exec generateTestUser @USERNAME = 'nportman', @FIRSTNAME = 'Natalie', @LASTNAME = 'Portman', @GRADE = 9, @FISCAL_YEAR = 2022, @PARTY_ID = 111;
 
 go
 
