188eaeaddb0bebe50402c15cde046429c1ecef88
Evangelos Poneres <evangelos@even.health>
2023-01-31 11:52:35 -0500

    update lead detail extract

--- LEAD_DETAIL_EXTRACT.sql
+++ LEAD_DETAIL_EXTRACT.sql
@@ -2,12 +2,14 @@ DECLARE @ELEMENT_ID VARCHAR(255) = 'ADJ'
 DECLARE @FISCAL_YEAR INT = 2023
 
 begin try drop table #tmp_review end try begin catch end catch
-select top 100 * 
+select r.*, le.* 
 into #tmp_review
 from review r
 inner join lead_element le
 on r.element_id = le.id
-where review_section = 'QUALITY' and le.lead_element_id = @ELEMENT_ID
+inner join review_period rp
+on r.review_period_id = rp.id
+where review_section = 'QUALITY' and rp.review_month in (10, 11, 12) and rp.review_year = 2022 and le.lead_element_id = @ELEMENT_ID
 
 begin try drop table #tmp_results end try begin catch end catch
 select r.review_id
@@ -34,12 +36,12 @@ inner join review_evaluation re
 on rp.review_evaluation_id = re.id
 left join [user] ce
 on re.user_id = ce.user_id
-left join ce_supervisor_dashboard_user super_ce
-on ce.user_id = super_ce.claims_examiner_id
-left join [user] super
-on super_ce.ce_supervisor_id = super.user_id
 left join ifecs_case ic
 on r.case_id = ic.case_id
+left join review_summary rs
+on r.review_period_id = rs.review_period_id
+left join [user] super
+on rs.updated_by = super.user_id
 
 begin try drop table #tmp_lead_questions end try begin catch end catch
 select question_number, question_text
@@ -62,13 +64,6 @@ BEGIN
     BEGIN
         EXEC('ALTER TABLE #tmp_results ADD [' + @Q_COL+ '] VARCHAR(255)')
     END
-
-    -- DECLARE @C_COL VARCHAR(255) = (SELECT 'COMMENTS_' + CAST(@QUESTION_NUMBER as VARCHAR))
-    -- IF COL_LENGTH('#tmp_results', @C_COL) IS NULL
-    -- BEGIN
-    --     EXEC('ALTER TABLE #tmp_results ADD [' + @C_COL+ '] VARCHAR(255)')
-    -- END
-
 
     FETCH NEXT FROM db_cursor INTO @QUESTION_NUMBER, @QUESTION_TEXT
 END 
@@ -122,12 +117,6 @@ BEGIN
         select @SQL =  @SQL + 'update #tmp_results set Q_' + CAST(question_number as VARCHAR) + ' = ''' + response + ''' where review_id = ' + cast(@REVIEW_ID as VARCHAR(255))
         from #tmp_questions_and_answers
         exec(@SQL)
-
-        -- set @SQL = ''
-        -- select @SQL =  @SQL + 'update #tmp_results set COMMENTS_' + CAST(question_number as VARCHAR) + ' = ''' + comments + ''' where review_id = ' + cast(@REVIEW_ID as VARCHAR(255))
-        -- from #tmp_questions_and_answers
-        -- exec(@SQL)
-
     end
 
     FETCH NEXT FROM db_cursor INTO @REVIEW_ID, @DETAILS_JSON
